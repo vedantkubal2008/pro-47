a0a4e8d3aced0c8a4d871f47d3b4d57e74da4186
vedantkubal2008 <74858673+vedantkubal2008@users.noreply.github.com>
2021-03-29 15:02:37 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pro-47
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ sketch.js
@@ -0,0 +1,277 @@
+var canva;
+var gameState="START";
+var passer_6;
+var mn=0,sc
+var score=0;
+var key1,key2,key3;
+function preload(){
+  c_door =loadImage("Images/closed_door.jpg");
+  o_door = loadImage("Images/Opened_door.jpg");
+  c =loadImage("Images/cor.jpg");
+  house = loadImage("Images/H_house.jpg");
+  hall=loadImage("Images/Hall2.jpg");
+  Man= loadImage("Images/m.png");
+  r1=loadImage("Images/Room_1.jpg");
+  r2=loadImage("Images/Room_2.jpg");
+  r3=loadImage("Images/Room_3.jpg");
+  r4=loadImage("Images/Room_4.jpg");
+  obj=loadImage("Images/key.png");
+  badge= loadImage("Images/Badge.png");
+}
+
+function setup(){
+  canvas = createCanvas(1000,800);
+  man = createSprite(310,600);
+  man.addImage(Man);
+  man.scale=0.1;
+  passer_1=createSprite(482,586,10,10);
+  passer_1.visible=false;
+  input=createInput("Enter code .Hint:Capital of Brazil");
+   input2=createInput("Enter code to get keys.Enter the previous code");
+  man.setCollider("rectangle",0,0,500,1200);//-150,0,500,1200
+  man.debug = false
+    key1=createSprite(330,130,10,10);
+    key1.addImage(obj);
+    key1.scale=0.03;
+    key1.visible=false;
+ key2=createSprite(840,600,10,10);//480,585
+  key2.addImage(obj);
+  key2.scale=0.03;
+  key2.visible=false;
+
+  key3=createSprite(640,385,10,10);//480,585
+  key3.addImage(obj);
+  key3.scale=0.03;
+  key3.visible=false;
+
+  key4=createSprite(110,540,10,10);//480,585
+  key4.addImage(obj);
+  key4.scale=0.03;
+  key4.visible=false;
+
+  key5=createSprite(830,130,10,10);//480,585
+  key5.addImage(obj);
+  key5.scale=0.03;
+  key5.visible=false;
+  key6=createSprite(5,90,10,10);//480,585
+  key6.addImage(obj);
+  key6.scale=0.02;
+  key6.visible=false;
+
+  key7=createSprite(290,5,10,10);//480,585
+  key7.addImage(obj);
+  key7.scale=0.02;
+  key7.visible=false;
+  key8=createSprite(990,650,10,10);//480,585
+  key8.addImage(obj);
+  key8.scale=0.02;
+  key8.visible=false;
+  key9=createSprite(995,5,10,10);//480,585
+  key9.addImage(obj);
+  key9.scale=0.02;
+  key9.visible=false;
+  ba=createSprite(500,400,100,100);
+  ba.addImage(badge)
+  ba.visible=false
+}
+
+
+function draw(){
+  background(house);
+  text(mouseX+','+mouseY,mouseX,mouseY);
+  textSize(40);
+  fill("red");
+  text("--Fight For Badge--",300,50);
+  console.log(score);
+  
+  if ( gameState==="START" ){
+    if(keyDown("s")){
+
+    
+    man.y = man.y-1;
+    man.x= man.x+8
+    man.scale=  man.scale-0.002;
+    }
+   
+    
+}
+//if(gameState==="PLAY"){
+  if(man.isTouching(passer_1)){
+    gameState="PLAY";
+     background(c_door);
+ //    text(mouseX+','+mouseY,mouseX,mouseY);
+     man.scale=0.3;
+     input.style('1500', '700px');
+     input.position(310,130);
+     fill("yellow")
+     textSize(30)
+    text("Keys collected : "+score,600,50)
+  }
+  var abc=input.value();
+  if(abc==="Brasilia"){
+    passer_1.destroy();
+    input.hide();
+
+     background(o_door);
+   //  text(mouseX+','+mouseY,mouseX,mouseY);
+     man.scale=0.4;
+
+     if (keyDown(UP_ARROW)){
+      man.y = man.y-1;
+     // man.x= man.x+1
+
+      man.scale=  man.scale-0.002;
+     }
+     fill("yellow")
+     textSize(30)
+    text("Keys collected : "+score,600,50)
+  }
+ var passer_2 = createSprite(470,150,100,50);
+  passer_2.visible=false;
+  if (man.isTouching(passer_2)){
+    background(c);
+    //text(mouseX+','+mouseY,mouseX,mouseY);
+    man.scale=0.3;
+  man_movement();
+  passer_2.destroy();
+  fill("yellow")
+  textSize(30)
+ text("Keys collected : "+score,600,50)
+}
+
+  
+ 
+ 
+
+passer_6 =createSprite(510,170,1000,10)//520,265,1,1
+  passer_6.visible=true;
+  passer_6.shapeColor="blue";
+if (man.isTouching(passer_6)){
+  background(r3);
+  text("Collect 3 keys to go to next room",30,100);
+  key1.visible=true;
+  key2.visible=true;
+  key3.visible=true;
+ man.setCollider("rectangle",0,0,500,1400)
+  if(man.isTouching(key1)){
+   
+      key1.destroy();
+      score++;
+       console.log("I'm here")
+  }
+  if(man.isTouching(key2)){
+   
+    key2.destroy();
+    score++;
+     console.log("I'm here")
+}
+if(man.isTouching(key3)){
+   
+  key3.destroy();
+  score++;
+   console.log("I'm here")
+}
+  passer_6.width=1000;
+  passer_6.height=1000;
+  
+  
+var invsprite=createSprite(150,6800,5,700);
+invsprite.shapeColor="blue";
+man.collide(invsprite)
+
+invsprite.visible=true;
+  man.scale=0.4;
+console.log(man.x);
+  //text(mouseX+','+mouseY,mouseX,mouseY);
+  man_movement();
+  fill("yellow")
+  textSize(30)
+ text("Keys collected : "+score,600,50)
+    if (score>=3){
+       background(r1)
+       man.scale=0.4
+       fill("yellow")
+       textSize(30)
+      text("Keys collected : "+score,600,50)
+     fill("red")
+     textSize(20);
+    text("collect 2 key to got to the final room",30,100)
+   // text(mouseX+','+mouseY,mouseX,mouseY);
+    key4.visible=true;
+    key5.visible=true
+    if(man.isTouching(key4)){
+       key4.destroy();
+       score++;
+    }
+    if(man.isTouching(key5)){
+      key5.destroy();
+      score++;
+   }
+   if(score>=5){
+      background(r4);
+      man.scale=0.4
+      fill("yellow")
+      textSize(30)
+     text("Keys collected : "+score,600,50)
+  
+   //text(mouseX+','+mouseY,mouseX,mouseY);
+   
+input2.style('1500', '700px');
+     input2.position(150,50);
+     var xyz=input2.value(); 
+     if(xyz==="Brasilia"){
+   key6.visible=true;
+   key7.visible=true;
+   key8.visible=true;
+   key9.visible=true;
+   input2.hide();
+   if(man.isTouching(key6)){
+    key6.destroy();
+    score++;
+ }
+ if(man.isTouching(key7)){
+  key7.destroy();
+  score++;
+}
+if(man.isTouching(key8)){
+  key8.destroy();
+  score++;
+}
+if(man.isTouching(key9)){
+  key9.destroy();
+  score++;
+}
+     }
+if(score>=9){
+  background("black")
+   ba.visible=true
+   man.visible=false;
+   textSize(35);
+  text("You won!! This badge is yours",300,700)
+}
+   }
+    }
+}
+
+ passer_6.destroy();
+ 
+
+ var edges= createEdgeSprites();
+   man.collide(edges);
+  drawSprites();
+  
+}
+function man_movement(){
+  if (keyDown(UP_ARROW)){
+    man.scale=  man.scale-0.02;
+ }
+ if (keyDown(RIGHT_ARROW)){
+    man.x =man.x+3;
+ }
+ if (keyDown(LEFT_ARROW)){
+   man.x =man.x-3;
+}
+if (keyDown(DOWN_ARROW)){
+    man.y =man.y+3;
+} 
+}
